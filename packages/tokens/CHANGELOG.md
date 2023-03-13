a8d7b7a4a01769e20953e2bc9f7a9b11cc5e16d0
joezerSmaniotto <joezersmaniotto5@gmail.com>
2023-03-13 16:28:47 -0300

    fix: Update name color

--- packages/tokens/CHANGELOG.md
+++ packages/tokens/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @joezer-ignite-ui/tokens
 
+## 2.1.0
+
+### Minor Changes
+
+- Update name color teste from test
+
 ## 2.0.0
 
 ### Major Changes
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -15,5 +15,5 @@ export const colors = {
   ignite500: '#00875F',
   ignite700: '#015F43',
   ignite900: '#00291D',
-  teste: '#FFF',
+  test: '#FFF',
 } as const
